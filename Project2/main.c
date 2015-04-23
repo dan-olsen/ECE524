d58e3cdaac7dfae69242a74b89344927bf396306
Daniel Olsen <dolsen@siu.edu>
2015-04-23 10:25:55 -0500

    Fix whitespace of given code

--- Project2/main.c
+++ Project2/main.c
@@ -11,9 +11,9 @@ Header Files
 /***************************************************************************************************************************
 Constant Declarations 
 ****************************************************************************************************************************/
-#define DELIMITER 1
-#define VARIABLE 2
-#define NUMBER 3
+#define DELIMITER 	1
+#define VARIABLE 	2
+#define NUMBER 		3
 #define MAX_LINE	100
 
 char token[80];
@@ -22,8 +22,7 @@ char *prog;
 /***************************************************************************************************************************
 Structure Declarations 
 ****************************************************************************************************************************/
-typedef struct struct_node
-{
+typedef struct struct_node {
 	char nd_num;
 	int refe;
 	int oper;
@@ -46,103 +45,110 @@ int isdelim(char);
 
 void main(int argc,char **argv) 
 {
-FILE *infile, *resfile;
-node graph[100];
+	FILE *infile, *resfile;
+	node graph[100];
 
-int i=0, counter, Mnode, Mnode_id=0;
-int mult_count=0, alu_count=0, oper_count=0;
-int Nasap, Fasap, C=1, min_asap, flag, sameasap =1;
+	int i=0, counter, Mnode, Mnode_id=0;
+	int mult_count=0, alu_count=0, oper_count=0;
+	int Nasap, Fasap, C=1, min_asap, flag, sameasap =1;
 
-char line[100], buf[100];
+	char line[100], buf[100];
 
-infile = fopen(argv[1], "r");
+	infile = fopen(argv[1], "r");
 
-while(fgets(buf, MAX_LINE, infile)!='\0')
-  {
-	prog=buf;
-	counter=0;
-	do
-	{
-		get_token();
-		switch(tok_type)
+	while(fgets(buf, MAX_LINE, infile)!='\0')
+  	{
+		prog=buf;
+		counter=0;
+		do
 		{
-			case NUMBER:
-				if (counter==0)
-				{
-				graph[i].nd_num= atoi(token);
-				if (Mnode_id < graph[i].nd_num) Mnode_id = graph[i].nd_num;
-				}
-				if (counter ==1)
-				{
-				graph[i].refe = atoi(token);
-				}
-				if (counter == 2)
-				{
-				graph[i].asap = atoi(token);
-				if (i==0)	min_asap = graph[i].asap;
-				else if(graph[i].asap < min_asap)	min_asap = graph[i].asap;
-				}
-				if (counter==3)
-				{
-				graph[i].alap = atoi(token);
-				}
-				counter++;
-			break;
-
-			case VARIABLE:
-				if (counter < 2)
-				{
-					if(! strcmp(token, "source"))
+			get_token();
+			switch(tok_type)
+			{
+				case NUMBER:
+					if (counter==0)
 					{
-					graph[i].oper=0;
+						graph[i].nd_num= atoi(token);
+
+						if (Mnode_id < graph[i].nd_num)
+							Mnode_id = graph[i].nd_num;
 					}
-					if (! strcmp (token, "sink"))
+
+					if (counter ==1)
 					{
-					graph[i].oper=3;
+						graph[i].refe = atoi(token);
 					}
-				}
-			break;
 
-			case DELIMITER:
-				if (counter > 0 && counter < 2)	
-				{	
-					if (strchr("*", *token))
+					if (counter == 2)
 					{
-					graph[i].oper=1;
-					mult_count++;
+						graph[i].asap = atoi(token);
+						if (i==0)
+							min_asap = graph[i].asap;
+						else if(graph[i].asap < min_asap)
+							min_asap = graph[i].asap;
 					}
-					if (strchr("+-/%^=()<>",*token))
+
+					if (counter==3)
 					{
-					graph[i].oper=2;
-					alu_count++;
+						graph[i].alap = atoi(token);
 					}
-				}
-			break;
+					counter++;
+				
+					break;
+				case VARIABLE:
+					if (counter < 2)
+					{
+						if(! strcmp(token, "source"))
+						{
+							graph[i].oper=0;
+						}
 
+						if(! strcmp (token, "sink"))
+						{
+							graph[i].oper=3;
+						}
+					}
+				
+					break;
+				case DELIMITER:
+					if (counter > 0 && counter < 2)	
+					{	
+						if (strchr("*", *token))
+						{
+							graph[i].oper=1;
+							mult_count++;
+						}
 
-
-			default:
-			break;
+						if (strchr("+-/%^=()<>",*token))
+						{
+							graph[i].oper=2;
+							alu_count++;
+						}
+					}
+				
+					break;
+				default:
+					break;
+			}
 		}
-	} 
-	while (counter < 4);
-	Mnode=i;
-	i++;
+
+		while (counter < 4);
+		Mnode=i;
+		i++;
 	
-  } 
+	} 
 
 	resfile = fopen ("equations.lp", "w");
 
-// 1. Read the benchmark files(.asapalap) and store it in your own defined data structure 
-// 2. Form the linear equations using the information in the data structure and print in a file(ilpfile) 
-// 3. Use lp solver to solve the inequalities and store the output of lpsolver in file.
-      //lpsover command: lp_solve "ilpfile" 
-// 4. The two functions(get_token,isdelim) can be used for reading the benchmark files(.asapalap). 
-      //Using these two functions in your implementation is optional. 
+	// 1. Read the benchmark files(.asapalap) and store it in your own defined data structure 
+	// 2. Form the linear equations using the information in the data structure and print in a file(ilpfile) 
+	// 3. Use lp solver to solve the inequalities and store the output of lpsolver in file.
+		  //lpsover command: lp_solve "ilpfile" 
+	// 4. The two functions(get_token,isdelim) can be used for reading the benchmark files(.asapalap). 
+		  //Using these two functions in your implementation is optional. 
 
-
-fclose(infile);
-fclose(resfile);
+	fclose(infile);
+	fclose(resfile);
 }//end of main
 /***************************************************************************************************************************
  Read a group of characters together return as a single string refered as token
@@ -150,31 +156,36 @@ fclose(resfile);
 ****************************************************************************************************************************/
 void get_token(void)
 {
-register char *temp;
+	register char *temp;
 
-tok_type = 0;
-temp = token;
-*temp = '\0';
-//printf("here");
-if(!*prog) return; /* at end of expression */
+	tok_type = 0;
+	temp = token;
+	*temp = '\0';
+	//printf("here");
+	if(!*prog)
+		return; /* at end of expression */
 
-while(isspace(*prog))  ++prog; /* skip over the white space */
+	while(isspace(*prog))
+		++prog; /* skip over the white space */
 
-if(strchr("<+-*/%^=()", *prog) ){
-  tok_type = DELIMITER;
-  /* advance to the next char */
-  *temp++ = *prog++;  }
-
-else if( isalpha(*prog) ){
-  while((!isdelim(*prog))&&(!isspace(*prog))  )
-    *temp++ = *prog++;
-    tok_type = VARIABLE;  }
+	if(strchr("<+-*/%^=()", *prog) ){
+	  tok_type = DELIMITER;
+	  /* advance to the next char */
+	  *temp++ = *prog++;  
 	
-else if( isdigit(*prog) ) {
-  while( (!isdelim(*prog))&&(!isspace(*prog)) )
-    *temp++ = *prog++;
-    tok_type = NUMBER;   }
-*temp = '\0';
+	} else if( isalpha(*prog) ){
+	  while((!isdelim(*prog))&&(!isspace(*prog))  )
+		*temp++ = *prog++;
+		tok_type = VARIABLE;
+
+	} else if( isdigit(*prog) ) {
+	  while( (!isdelim(*prog))&&(!isspace(*prog)) )
+		*temp++ = *prog++;
+		tok_type = NUMBER;
+
+	}
+
+	*temp = '\0';
 }//end
 /***************************************************************************************************************************
  Return true if c is a delimiter
@@ -182,7 +193,9 @@ else if( isdigit(*prog) ) {
 ****************************************************************************************************************************/
 int isdelim(char c)
 {
-if( strchr("<+-/*%^=()", c) || c==9 || c==0 || c=='\r' )  return 1;
-return 0;
+	if( strchr("<+-/*%^=()", c) || c==9 || c==0 || c=='\r' )
+		return 1;
+	else
+		return 0;
 }//end
 /***************************************************************************************************************************/
