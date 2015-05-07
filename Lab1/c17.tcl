9003069b189c5dc4875bb67a2bc37829fdd41d35
Daniel Olsen <dolsen@siu.edu>
2015-05-06 20:37:01 -0500

    Update scripts

--- Lab1/c17.tcl
+++ Lab1/c17.tcl
@@ -1,10 +1,10 @@
 set_attribute lib_search_path /cadence/GPDK/osu_stdcells/lib/tsmc018/signalstorm
 set_attribute library osu018_stdcells.lib
-read_hdl -vhdl FullAdder.vhd
+read_hdl -verilog c17.v
 elaborate
 synthesize -to_generic
 synthesize -to_mapped
-write_hdl -generic FullAdder > FullAdderGen.v
-write_hdl -mapped FullAdder > FullAdderMap.v
-report area > areaFA.rep
-rm FullAdder
+write_hdl -generic c17 > c17Gen.v
+write_hdl -mapped c17 > c17Map.v
+report area > areaC17.rep
+rm c17
--- Lab1/c6288.tcl
+++ Lab1/c6288.tcl
@@ -1,10 +1,10 @@
 set_attribute lib_search_path /cadence/GPDK/osu_stdcells/lib/tsmc018/signalstorm
 set_attribute library osu018_stdcells.lib
-read_hdl -vhdl FullAdder.vhd
+read_hdl -verilog c6288.v
 elaborate
 synthesize -to_generic
 synthesize -to_mapped
-write_hdl -generic FullAdder > FullAdderGen.v
-write_hdl -mapped FullAdder > FullAdderMap.v
-report area > areaFA.rep
-rm FullAdder
+write_hdl -generic c6288 > c6288Gen.v
+write_hdl -mapped c6288 > c6288Map.v
+report area > areaC6288.rep
+rm c6288
