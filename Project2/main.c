8fbcf13a3ca6002bb71b960bbe5e39f17d090549
Daniel Olsen <dolsen@siu.edu>
2015-05-07 14:54:51 -0500

    Almost finish project 2

--- Project2/main.c
+++ Project2/main.c
@@ -16,6 +16,11 @@ Constant Declarations
 #define NUMBER 		3
 #define MAX_LINE	100
 
+#define SOURCE 0
+#define MULT 1
+#define ALU 2
+#define SINK 3
+
 /***************************************************************************************************************************
 Structure Declarations
 ****************************************************************************************************************************/
@@ -40,13 +45,16 @@ Function Declarations
 ****************************************************************************************************************************/
 void get_token(void);
 int isdelim(char);
-void TimeConstraints(FILE *resfile, int Mnode);
+void TimeConstraints(FILE *resfile, int *c, int min_asap, int Mnode);
+void MultOperations(FILE *resFile, int *c, int min_asap, int Mnode);
+void AluOperations(FILE *resFile, int *c, int min_asap, int Mnode);
+void MultipleTimeFrames(FILE *resFile, int min_asap, int Mnode);
 
 /***************************************************************************************************************************
   Main Function
 ****************************************************************************************************************************/
 
-void main(int argc,char **argv)
+int main(int argc,char **argv)
 {
 	FILE *infile, *resfile;
 
@@ -54,7 +62,7 @@ void main(int argc,char **argv)
     int mult_count = 0, alu_count = 0, oper_count = 0;
     int Nasap, Fasap, C = 1, min_asap, flag, sameasap = 1;
 
-    char line[100], buf[100];
+    char resfileName[100], buf[100], LpCommand[100];
 
 	infile = fopen(argv[1], "r");
 
@@ -105,10 +113,10 @@ void main(int argc,char **argv)
                     if(counter < 2)
                     {
                         if(!strcmp(token, "source"))
-                            graph[i].oper = 0;
+                            graph[i].oper = SOURCE;
 
                         if(!strcmp(token, "sink"))
-                            graph[i].oper = 3;
+                            graph[i].oper = SINK;
                     }
 
                     break;
@@ -117,13 +125,13 @@ void main(int argc,char **argv)
                     {
                         if(strchr("*", *token))
                         {
-                            graph[i].oper = 1;
+                            graph[i].oper = MULT;
                             mult_count++;
                         }
 
                         if(strchr("+-/%^=()<>", *token))
                         {
-                            graph[i].oper = 2;
+                            graph[i].oper = ALU;
                             alu_count++;
                         }
                     }
@@ -138,7 +146,9 @@ void main(int argc,char **argv)
         i++;
     }
 
-	resfile = fopen ("equations.lp", "w");
+    sprintf(resfileName, "%s_equations.lp", argv[1]);
+
+	resfile = fopen (resfileName, "w");
 
 	// 1. Read the benchmark files(.asapalap) and store it in your own defined data structure 
 	// 2. Form the linear equations using the information in the data structure and print in a file(ilpfile) 
@@ -147,8 +157,34 @@ void main(int argc,char **argv)
 	// 4. The two functions(get_token,isdelim) can be used for reading the benchmark files(.asapalap). 
           //Using these two functions in your implementation is optional.
 
+    printf("min: a1 + a2;\n");
+    fprintf(resfile, "min: a1 + a2;\n");
+
+	TimeConstraints(resfile, &C, 2, Mnode);
+
+    printf("\n");
+    fprintf(resfile, "\n");
+
+    MultOperations(resfile, &C, 2, Mnode);
+
+    printf("\n");
+    fprintf(resfile, "\n");
+
+    AluOperations(resfile, &C, 2, Mnode);
+
+    printf("\n");
+    fprintf(resfile, "\n");
+
+    MultipleTimeFrames(resfile, 2, Mnode);
+
 	fclose(infile);
     fclose(resfile);
+
+    sprintf(LpCommand, "lp_solve %s > %s.output", resfileName, resfileName);
+
+    system(LpCommand);
+
+    return 0;
 }//end of main
 /***************************************************************************************************************************
  Read a group of characters together return as a single string refered as token
@@ -202,17 +238,195 @@ int isdelim(char c)
 		return 0;
 }//end
 
-void TimeConstraints(FILE *resfile, int Mnode)
+void TimeConstraints(FILE *resfile, int *c, int min_asap, int Mnode)
 {
-    int i;
+    int i, j;
 
-    for(i = 0; i < Mnode; i++)
+    for(i = 0; i < Mnode; i++, (*c)++)
     {
         if(graph[i].alap == graph[i].asap)
         {
-            fprintf(resfile, "c%d: X%d%d = 1;", c, i, graph[i].asap);
+            printf("c%d: X%d%d = 1;\n", *c, (int)graph[i].nd_num, graph[i].asap - min_asap);
+
+            fprintf(resfile, "c%d: X%d%d = 1;\n", *c, (int)graph[i].nd_num, graph[i].asap - min_asap);
+        } else {
+			printf("c%d: ", *c);
+
+			fprintf(resfile, "c%d: ", *c);
+
+        	for(j = graph[i].asap - min_asap; j <= (graph[i].alap - min_asap); j++)
+        	{
+        		if(j == (graph[i].alap - min_asap))
+        		{
+        			printf("X%d%d", (int)graph[i].nd_num, j);
+
+        			fprintf(resfile, "X%d%d", (int)graph[i].nd_num, j);
+        		} else {
+        			printf("X%d%d + ", (int)graph[i].nd_num, j);
+
+        			fprintf(resfile, "X%d%d + ", (int)graph[i].nd_num, j);
+        		}
+        	}
+
+        	printf(" = 1;\n");
+        	fprintf(resfile, "= 1;\n");
         }
     }
 }
 
+void MultOperations(FILE *resFile, int *c, int min_asap, int Mnode)
+{
+	int i, j, Mmult, Mmult_at_time_frame = 0, k;
+
+	for(i = 0; i < Mnode; i++)
+	{
+		if(graph[i].oper == MULT)
+		{
+			Mmult = graph[i].alap - min_asap;
+		}
+	}
+
+	for(j = 1; j <= Mmult; j++, (*c)++, Mmult_at_time_frame = 0)
+	{
+		for(i = 0; i < Mnode; i++)
+		{
+			if(graph[i].oper == MULT && j >= (graph[i].asap - min_asap) && j <= (graph[i].alap - min_asap))
+			{
+				Mmult_at_time_frame++;
+			}
+		}
+
+		if(Mmult_at_time_frame == 0)
+			continue;
+
+        printf("c%d: ", *c);
+
+        fprintf(resFile, "c%d: ", *c);
+
+		for(i = 0, k = 1; k <= Mmult_at_time_frame; i++)
+		{
+			if(graph[i].oper == MULT)
+			{
+		        if(j >= (graph[i].asap - min_asap) && j <= (graph[i].alap - min_asap))
+		        {
+		        	if(k == Mmult_at_time_frame)
+		        	{
+						printf("X%d%d ", (int)graph[i].nd_num, j);
+
+						fprintf(resFile, "X%d%d ", (int)graph[i].nd_num, j);
+		        	} else {
+						printf("X%d%d + ", (int)graph[i].nd_num, j);
+
+						fprintf(resFile, "X%d%d + ", (int)graph[i].nd_num, j);
+		        	}
+
+					k++;
+		        }
+			}
+		}
+
+		printf("- a1 <= 0;\n");
+
+		fprintf(resFile, "- a1 <= 0;\n");
+	}
+}
+
+void AluOperations(FILE *resFile, int *c, int min_asap, int Mnode)
+{
+	int i, j, Malu, Malu_at_time_frame = 0, k;
+
+	for(i = 0; i < Mnode; i++)
+	{
+		if(graph[i].oper == ALU)
+		{
+			Malu = graph[i].alap - min_asap;
+		}
+	}
+
+	for(j = 1; j <= Malu; j++, (*c)++, Malu_at_time_frame = 0)
+	{
+		for(i = 0; i < Mnode; i++)
+		{
+			if(graph[i].oper == ALU && j >= (graph[i].asap - min_asap) && j <= (graph[i].alap - min_asap))
+			{
+				Malu_at_time_frame++;
+			}
+		}
+
+		if(Malu_at_time_frame == 0)
+			continue;
+
+        printf("c%d: ", *c);
+
+        fprintf(resFile, "c%d: ", *c);
+
+		for(i = 0, k = 1; k <= Malu_at_time_frame; i++)
+		{
+			if(graph[i].oper == ALU)
+			{
+		        if(j >= (graph[i].asap - min_asap) && j <= (graph[i].alap - min_asap))
+		        {
+		        	if(k == Malu_at_time_frame)
+		        	{
+						printf("X%d%d ", (int)graph[i].nd_num, j);
+
+						fprintf(resFile, "X%d%d ", (int)graph[i].nd_num, j);
+		        	} else {
+						printf("X%d%d + ", (int)graph[i].nd_num, j);
+
+						fprintf(resFile, "X%d%d + ", (int)graph[i].nd_num, j);
+		        	}
+
+		        	k++;
+		        }
+			}
+		}
+
+		printf("- a2 <= 0;\n");
+
+		fprintf(resFile, "- a2 <= 0;\n");
+	}
+}
+
+void MultipleTimeFrames(FILE *resFile, int min_asap, int Mnode)
+{
+    int i, j, Mmult_time_frames, k;
+
+	printf("int a1, a2, ");
+
+	fprintf(resFile, "int a1, a2, ");
+
+    for(i = 0, Mmult_time_frames = 0; i < Mnode; i++)
+    {
+        if(graph[i].alap != graph[i].asap)
+        {
+        	Mmult_time_frames++;
+        }
+    }
+
+    for(i = 0, k = 1; k <= Mmult_time_frames; i++)
+    {
+        if(graph[i].alap != graph[i].asap)
+        {
+        	for(j = graph[i].asap - min_asap; j <= (graph[i].alap - min_asap); j++)
+        	{
+        		if(k == Mmult_time_frames && j == (graph[i].alap - min_asap))
+        		{
+        			printf("X%d%d", (int)graph[i].nd_num, j);
+
+        			fprintf(resFile, "X%d%d", (int)graph[i].nd_num, j);
+        		} else {
+        			printf("X%d%d, ", (int)graph[i].nd_num, j);
+
+        			fprintf(resFile, "X%d%d, ", (int)graph[i].nd_num, j);
+        		}
+        	}
+
+        	k++;
+        }
+    }
+
+	printf(";\n");
+	fprintf(resFile, ";\n");
+}
 /***************************************************************************************************************************/
